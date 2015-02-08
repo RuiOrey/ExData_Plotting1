f1ab6f2f622931d0910c95dc3c1ef12d6beb0b11
RuiOrey <rui.albuquerque.dorey@gmail.com>
2015-02-08 22:52:34 +0000

    Plot 2

--- plot2.R
+++ plot2.R
@@ -1,6 +1,6 @@
 ##Dataset read
 data <- read.csv("household_power_consumption.txt", header=T, sep=';', na.strings="?", 
-                      nrows=2075259, check.names=F, stringsAsFactors=F, comment.char="", quote='\"')
+                 nrows=2075259, check.names=F, stringsAsFactors=F, comment.char="", quote='\"')
 data$Date <- as.Date(data$Date, format="%d/%m/%Y")
 ## Subset
 data <- subset(data, subset=(Date >= "2007-02-01" & Date <= "2007-02-02"))
@@ -8,8 +8,9 @@ data <- subset(data, subset=(Date >= "2007-02-01" & Date <= "2007-02-02"))
 datetime <- paste(as.Date(data$Date), data$Time)
 data$Datetime <- as.POSIXct(datetime)
 
-## Plot 1
-png(file="plot1.png",width=480,height=480)
-hist(data$Global_active_power,xlab = "Global Active Power (killowats)",main = "Global Active Power",col="red")
+## Plot 2
+png(file="plot2.png",width=480,height=480)
+Sys.setlocale("LC_TIME", "C")
+plot(data$Datetime,data$Global_active_power,ylab = "Global Active Power(kilowatts)",main = "",type = "l",xlab = "")
 dev.off()
 
